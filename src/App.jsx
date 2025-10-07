60f06f4cede5c4eb3eae764a13d5b8f692ec29d0
Jatin <jatinverma2428@gmail.com>
2025-10-07 17:23:58 +0530

    kk

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
-import Header from './components/Header'
-import Footer from './components/Footer'
+// import React, { Children } from 'react'
+// import Header from './components/Header'
+// import Footer from './components/Footer'
 import MainSection from './pages/MainSection'
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom'
+import Jatin from './components/Jatin'
+import About from './pages/About'
+import Contact from './pages/Contact'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Jatin />,
+
+    children: [
+      {
+        index: true,
+        element: <MainSection />
+      },
+      {
+        path: "about",
+        element: <About />
+      },
+      {
+        path: "contact",
+        element: <Contact />
+      }
+    ]
+  }
+])
 
 export default function App() {
   return (
     <>
-      <Header />
-      <MainSection />
-      <Footer />
+      <RouterProvider router={router} />
+
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,45 +2,46 @@ import React, { useState } from "react";
 import { FaShoppingCart } from "react-icons/fa";
 import { IoMdLogIn } from "react-icons/io";
 import { FaBars, FaTimes } from "react-icons/fa";
+import { Link } from "react-router-dom"
 
 export default function Header() {
-  const [open, setOpen] = useState(false);
+    const [open, setOpen] = useState(false);
 
-  return (
-    <header className="bg-amber-700 text-white px-8 py-5 shadow-md">
-      <div className="flex justify-between items-center">
-        <h1 className="text-2xl font-bold">Logo</h1>
+    return (
+        <header className="bg-amber-700 text-white px-8 py-5 shadow-md">
+            <div className="flex justify-between items-center">
+                <h1 className="text-2xl font-bold">Logo</h1>
 
-      
-        <button
-          className="text-2xl md:hidden"
-          onClick={() => setOpen(!open)}
-        >
-          {open ? <FaTimes /> : <FaBars />}
-        </button>
 
-        
-        <nav className="hidden md:flex items-center gap-10 text-lg">
-          <a href="#">Home</a>
-          <a href="#">About</a>
-          <a href="#">Contact</a>
-          <a href="#"><FaShoppingCart /></a>
-          <a href="#"><IoMdLogIn /></a>
-        </nav>
-      </div>
+                <button
+                    className="text-2xl md:hidden"
+                    onClick={() => setOpen(!open)}
+                >
+                    {open ? <FaTimes /> : <FaBars />}
+                </button>
 
-      
-      {open && (
-        <nav className="flex flex-col items-start gap-4 mt-4 md:hidden text-lg">
-          <a href="#">Home</a>
-          <a href="#">About</a>
-          <a href="#">Contact</a>
-          <div className="flex gap-5 text-xl">
-            <a href="#"><FaShoppingCart /></a>
-            <a href="#"><IoMdLogIn /></a>
-          </div>
-        </nav>
-      )}
-    </header>
-  );
+
+                <nav className="hidden md:flex items-center gap-10 text-lg">
+                    <Link to="/">Home</Link>
+                    <Link to="about">About</Link>
+                    <Link to="contact">Contact</Link>
+
+                    <a href="#"><FaShoppingCart /></a>
+                    <a href="#"><IoMdLogIn /></a>
+                </nav>
+            </div>
+
+
+            {open && (
+                <nav className="flex flex-col items-start gap-4 mt-4 md:hidden text-lg">
+                    <Link to="about">About</Link>
+                    <Link to="contact">Contact</Link>
+                    <div className="flex gap-5 text-xl">
+                        <a href="#"><FaShoppingCart /></a>
+                        <a href="#"><IoMdLogIn /></a>
+                    </div>
+                </nav>
+            )}
+        </header>
+    );
 }
